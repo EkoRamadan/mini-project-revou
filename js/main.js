040573c26636ecba5aad0bda80a178bc0191bc7f
EkoRamadan <mysitelandingpage@gmail.com>
2023-08-30 20:31:25 +0700

    24

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const tggl = document.querySelector(".tggl-nav")
+const nav = document.querySelector(".navigasi")
+tggl.addEventListener("click",()=>{
+    nav.classList.toggle("active")
+})
+
+const submit = document.getElementById("submit")
+const display = document.getElementById("name-header")
+const nameD = document.getElementById("name-display")
+const dateD = document.getElementById("date-display")
+const messageD = document.getElementById("message-display")
+const genderD = document.getElementById("gender-display")
+const headerD = document.getElementById("name-header")
+
+submit.addEventListener("click",(i)=>{
+    i.preventDefault()
+    const name = document.getElementById("name").value
+    const date = document.getElementById("date").value
+    const message = document.getElementById("message").value
+    const gender = document.querySelector("input[name='gender']:checked").value
+    
+    if(!name || !message  || !date || !gender) {
+        alert("lengkapi form")
+        return;
+    }
+    alert(`${gender}`)
+    nameD.innerHTML= name
+    headerD.innerHTML= name
+    dateD.innerHTML= date
+    messageD.innerHTML= message
+    genderD.innerHTML= gender
+})
